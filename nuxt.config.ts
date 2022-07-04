5b7c82dfe8cdd9d950e87edaee81c93177278c8f
Gityosan <ymo6190@docomo.ne.jp>
2022-07-05 07:35:08 +0900

    minifix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,22 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  typescript: {
+    shim: false
+    // strict: true
+  },
+  css: ['vuetify/styles'],
+  build: {
+    transpile: ['vuetify']
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false
+    },
+    server: {
+      watch: {
+        usePolling: true
+      }
+    }
+  }
 })
