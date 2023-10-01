a22942be1a96a1bfb14d54973e700bb3e90d5c04
Gityosan <ymo6190@docomo.ne.jp>
2023-10-01 13:30:48 +0900

    [fix] fix test

--- test/unit/atom-text.spec.ts
+++ test/unit/atom-text.spec.ts
@@ -1,4 +1,5 @@
 import { mount } from '@vue/test-utils'
+// @ts-ignore
 import AtomText from '@/components/atom/text/index.vue'
 const props = {
   text: 'sample',
@@ -9,7 +10,14 @@ const props = {
 }
 describe('AtomText', () => {
   const wrapper = mount(AtomText, { props })
+  const element = wrapper.find('p')
   test('data display', () => {
-    expect(wrapper.find('p').text()).toBe(props.text)
+    expect(element.text()).toBe(props.text)
+  })
+  test('class check', () => {
+    expect(element.attributes('class')).contain('text-subtitle-1')
+    expect(element.attributes('class')).contain('font-weight-bold')
+    expect(element.attributes('class')).contain('text-black')
+    expect(element.attributes('class')).contain('line-height-sm')
   })
 })
